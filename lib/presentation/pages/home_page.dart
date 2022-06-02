1c5b017a76941033f90f35881949396430424d01
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-06-02 16:29:28 +0530

    load data from json file

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -8,21 +11,29 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  Future readJson() async {
+    final String response = await rootBundle.loadString('assets/data.json');
+    final data = await jsonDecode(response);
+    print(data['products']);
+    return data['products'];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: TextButton(
-          onPressed: () async {},
-          child: Container(
-            alignment: Alignment.center,
-            width: 130,
-            child: const Text(
-              'Load Data',
-            ),
+          onPressed: () async {
+            var data = await readJson();
+            // save data to database
+          },
+          child: const Text(
+            'Load Data',
+            style: TextStyle(color: Colors.white, fontSize: 28.0),
           ),
           style: TextButton.styleFrom(
-            primary: const Color.fromARGB(255, 157, 11, 254),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            backgroundColor: const Color.fromARGB(255, 157, 11, 254),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(5.0),
               side: const BorderSide(
