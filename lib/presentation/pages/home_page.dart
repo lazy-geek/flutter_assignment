ed41b474bd75e6426750fba41c20d8859b16e638
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-06-02 15:53:26 +0530

    Add Button To Load Data

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -10,6 +10,28 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: () async {},
+          child: Container(
+            alignment: Alignment.center,
+            width: 130,
+            child: const Text(
+              'Load Data',
+            ),
+          ),
+          style: TextButton.styleFrom(
+            primary: const Color.fromARGB(255, 157, 11, 254),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5.0),
+              side: const BorderSide(
+                color: Color.fromARGB(255, 157, 11, 254),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
