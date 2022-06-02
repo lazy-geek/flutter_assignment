d93d0d1ec73f63dd827bd29aae6d6ade3fea96ce
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-06-02 20:45:33 +0530

    remove debug label

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Assignment',
+      debugShowCheckedModeBanner: false,
       home: Homepage(),
     );
   }
